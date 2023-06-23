1a56077825df03bc5093e746c84db3531ac7d292
quatermain1987 <quatermain1987@gmail.com>
2023-06-23 23:09:05 +0900

    commit

--- react-workspace/todo-react-app/src/App.js
+++ react-workspace/todo-react-app/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Todo from './Todo';
+import React, { useState } from "react";
 
 function App() {
+  const [itme, setItem] = useState({
+    id: "0",
+    title: "title1",
+    done: true
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Todo />
     </div>
   );
 }
---
+++ react-workspace/todo-react-app/src/Todo.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+const Todo = (props) => {
+    const [item, setItem] = useState(props.item);
+
+    return (
+        <div className="Todo">
+            <input 
+                type="checkbox" 
+                id={item.id} 
+                name={item.name} 
+                checked={item.done} 
+            />
+            <label id={item.id}>{item.title}</label>
+        </div>
+    )
+}
+
+export default Todo;
