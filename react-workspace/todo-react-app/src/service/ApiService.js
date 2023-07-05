e63ef512a284ae4125327454a3555968e6906d49
quatermain1987 <quatermain1987@gmail.com>
2023-07-05 19:56:08 +0900

    errorRouter

--- react-workspace/todo-react-app/src/service/ApiService.js
+++ react-workspace/todo-react-app/src/service/ApiService.js
@@ -1,13 +1,20 @@
 import { API_BASE_URL } from "../api-config";
 
 export function call(api, method, request) {
+    let headers = new Headers({
+        "Content-Type": "application/json",
+    })
+
+    const accessToken = localStorage.getItem("ACCESS_TOKEN");
+    if (accessToken && accessToken !== null) {
+        headers.append("Authorization", "Bearer" + accessToken);
+    }
+
     let options = {
-        headers: new Headers({
-            "Content-Type": "application/json",
-        }),
+        headers: headers,
         url: API_BASE_URL + api,
         method: method,
-    };
+    }
 
     if (request) {
         // GET Method
@@ -36,6 +43,9 @@ export function signin(userDTO) {
             console.log("response : ", response);
             alert("Login token : " + response.token);
             if (response.token) {
+                // 로컬 스토리지에 토큰 저장
+                localStorage.setItem("ACCESS_TOKEN", response.token);
+                // 토큰이 존재하면 Todo로 리디렉트
                 window.location.href = "/";
             }
         });
